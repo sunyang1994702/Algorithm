8addc38ccfa8a1bda23487777fe92e2db610cca0
sunyang1994702 <40493383+sunyang1994702@users.noreply.github.com>
2021-12-16 23:45:35 +0900

    Add files via upload

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,25 +1,36 @@
 """
-small demo judge if the arr is a palindrome
+    Find all single longest sub Palindrom array in the given array. 
+    Ex: [1,2,3,2,1,2]
+    return [1,2,3,2,1], [2,1,2]
+    The [2,3,2] has alrady exsited in [1,2,3,2,1]. So no need to return it again.
 """
 
+def subPalindrom(arr):
+    results = []
+    for i in range(len(arr)):
+        ## odd length:  aba
+        l, r = i-1, i+1
+        subArr = []     
+        while l >= 0 and r < len(arr) and arr[l] == arr[r]:
+            if (r - l + 1) > len(subArr):
+                subArr = arr[l:r+1]
+            l -= 1
+            r += 1
+        if subArr:
+            results.append(subArr)
+            
+        ## even length:  abba
+        l, r = i, i+1
+        subArr = []     
+        while l >= 0 and r < len(arr) and arr[l] == arr[r]:
+            if (r - l + 1) > len(subArr):
+                subArr = arr[l:r+1]
+            l -= 1
+            r += 1
+        if subArr:
+            results.append(subArr)
+    return results
 
-def isPalindrome(head):
-    i = 0
-    count = 0
-    head_len = len(head)    
-    while i <= (head_len/2):
-        if head[i] == head[head_len-i-1]:
-            i += 1
-            count = 1
-        else:
-            count = 0
-            break
-    
-    if count == 1:
-        print(True)
-    else:
-        print(False)
 
-
-if __name__ == '__main__':
-    isPalindrome([1,2,3,4,5,4,3,2,1])
+if __name__ == "__main__":
+    print(subPalindrom([1,2,3,4,3,2,1,2,14,2,2,14,19,200]))
