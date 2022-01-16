f46915f3be7d7318bcb780308583576ae20b9dda
sunyang1994702 <40493383+sunyang1994702@users.noreply.github.com>
2022-01-16 14:48:04 +0900

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -1,8 +1,9 @@
 """
 Two Sum II - Input Array Is Sorted
 Link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
-"""
 
+Time complexity: O(n)
+"""
 
 def twoSum(self, numbers: List[int], target: int) -> List[int]:
     ## Idea: using two pointers.
@@ -20,3 +21,6 @@ def twoSum(self, numbers: List[int], target: int) -> List[int]:
             r -= 1
     
     return None
+
+if __name__ == '__main__':
+    print(twoSum([1,3,4,5,7,10,11], 9))
